e21aec1b0cfd483c8f23f072c2a8f9dfeb97bd73
Kyle Massey <kyle.massey@gmail.com>
2023-10-26 23:56:31 -0400

    - Add CC list

--- email_alerter/constants.py
+++ email_alerter/constants.py
@@ -4,3 +4,5 @@ TSC_SITE_NAME = "kjmdev797388"
 BASE_DS_SEARCH_URL = "https://10ax.online.tableau.com/#/site/kjmdev797388/search/datasources?order_by=relevancy:desc&search="
 
 EMAIL_FROM = "kyle.massey@gmail.com"
+
+CC_LIST = ["kyle.massey@gmail.com", "kyle@kylejmassey.com"]
